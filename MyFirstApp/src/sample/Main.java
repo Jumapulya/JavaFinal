05672f69d4e6e1cf24efcbe346f721b699b79278
Jumapulya <44026196+Jumapulya@users.noreply.github.com>
2019-05-23 17:16:13 +0300

    Add files via upload

---
+++ MyFirstApp/src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("TopLevel Diary");
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("file:/home/anonym/IdeaProjects/MyFirstApp/icon2.png"));
+        primaryStage.show();
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
